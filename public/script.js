a44a6a94b8231116bf4fe311e770aa055e31f6da
masafumi kato <xslash1105@gmail.com>
2020-11-28 23:29:05 +0900

    [add] skyway for rtc

--- public/script.js
+++ public/script.js
@@ -1,14 +1,97 @@
-const socket = io('/');
-const myPeer = new Peer(undefined, {
-    host: '/',
-    port: '3001',
+'use strict';
+
+let localStream = null;
+let peer = null;
+let existingCall = null;
+
+// navigatorはユーザーエージェントの情報を返す https://developer.mozilla.org/ja/docs/Web/API/Navigator
+// mediaDevicesは使用可能なメディアデバイスの情報を取得する（読取専用）
+// getUserMediaは要求された種類のメディア{video, audio}でMediaStreamを生成する許可をユーザーに求める
+navigator.mediaDevices.getUserMedia({video: false, audio: true})
+    .then(function (stream) {
+        // Success
+        console.log($('#my-video').get(0).srcObject);
+        console.log(stream);
+        $('#my-video').get(0).srcObject = stream;
+        localStream = stream;
+    }).catch(function (error) {
+    // Error
+    console.error('mediaDevice.getUserMedia() error:', error);
+    return;
 });
 
-// myPeer.on('open', id => {
-//     socket.emit('join-room', ROOM_ID, id);
-// })
-socket.emit('join-room', ROOM_ID, 10);
+peer = new Peer({
+    key: '<API KEY>',
+    debug: 3
+});
 
-socket.on('user-connected', userId => {
-    console.log('user connected' + userId);
-})
+// 自分自身のIDで映像をVideoにいれる。
+peer.on('open', function(){
+    $('#my-id').text(peer.id);
+});
+
+peer.on('error', function(err){
+    console.log('error');
+    alert(err.message);
+});
+
+peer.on('close', function(){
+    console.log('close');
+});
+
+peer.on('disconnected', function(){
+    console.log('disconnected');
+});
+
+$('#make-call').submit(function(e){
+    console.log('call pressed');
+    e.preventDefault();
+    const call = peer.call($('#callto-id').val(), localStream);
+    setupCallEventHandlers(call);
+});
+
+$('#end-call').click(function(){
+    existingCall.close();
+});
+
+peer.on('call', function(call){
+    console.log('call');
+    call.answer(localStream);
+    setupCallEventHandlers(call);
+});
+
+function setupCallEventHandlers(call){
+    if (existingCall) {
+        existingCall.close();
+    };
+
+    existingCall = call;
+
+    call.on('stream', function(stream){
+        addVideo(call,stream);
+        setupEndCallUI();
+        $('#their-id').text(call.remoteId);
+    });
+    call.on('close', function(){
+        removeVideo(call.remoteId);
+        setupMakeCallUI();
+    });
+}
+
+function addVideo(call,stream){
+    $('#their-video').get(0).srcObject = stream;
+}
+
+function removeVideo(peerId){
+    $('#'+peerId).remove();
+}
+
+function setupMakeCallUI(){
+    $('#make-call').show();
+    $('#end-call').hide();
+}
+
+function setupEndCallUI() {
+    $('#make-call').hide();
+    $('#end-call').show();
+}
